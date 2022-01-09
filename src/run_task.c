b8aa6f2e64a11fe05f83c489bfa0f7edbcda9979
Laure Runser <laure-runser@tutanota.com>
2022-01-09 16:53:07 +0100

    Saturnd : - some refactorisation - the tasks are run from a child (which creates another child) so that saturnd doesn't hang - writes the exit code and the time of execution after the task is executed

--- src/run_task.c
+++ src/run_task.c
@@ -66,29 +66,79 @@ void move_stdout_stderr(int id) {
     free(err);
 }
 
-void run_tasks(s_task **tasks, int nb_tasks){
+void write_run_info(int64_t time, uint16_t exitcode, uint64_t taskid) {
+    // append the time and exitcodes
+    char *path_dir = get_directory_id_path(taskid);
+    char *filename = "/runs";
+    char *path_runs = get_file_path(path_dir, filename);
+    int fd = open(path_runs, O_APPEND | O_CREAT, S_IRWXU);
+
+    BYTE buf[sizeof(int64_t) + sizeof(uint16_t)];
+    memcpy(buf, &time, sizeof(int64_t));
+    memcpy(buf+sizeof(int64_t), &exitcode, sizeof(uint16_t));
+
+    write(fd, buf, sizeof(int64_t) + sizeof(uint16_t));
+    close(fd);
+
+    // write the number of runs
+    filename = "/nb_runs";
+    char *path_nb = get_file_path(path_dir, filename);
+    fd = open(path_nb, O_RDWR);
+    uint32_t nb_runs;
+    if (fd < 0) { // file didn't exist
+        fd = open(path_nb, O_RDWR | O_CREAT, S_IRWXU);
+        nb_runs = 1;
+    } else { // file already existed
+        read(fd, &nb_runs, sizeof(uint32_t));
+        nb_runs += 1;
+    }
+     write(fd, &nb_runs, sizeof(uint32_t));
+    close(fd);
+
+    free(path_dir);
+    free(path_runs);
+    free(path_nb);
+}
+
+void run_one_task(s_task *task, uint64_t taskid) {
+    int f = fork();
+    if (f == -1) {
+        perror("Fork error");
+        exit(EXIT_FAILURE);
+    } else if (f == 0) { // fork and execute the task
+        move_stdout_stderr(taskid);
+        char **tab = get_char_from_string(task->command->argv, task->command->argc);
+        char *com = tab[0];
+        execvp(com,tab);
+    } else { // wait until task is finished to write execution time + return value
+        int status; // to store the return status of the child
+        uint16_t exitcode; // the value to write to the file
+        int64_t time_of_execution = time(NULL); // current time
+        waitpid(f, &status, 0);
+
+        if (WIFEXITED(status)) { // child sent a return value
+            exitcode = WEXITSTATUS(status);
+        } else { // child didn't send a return value
+            exitcode = 0xFFFF;
+        }
+        write_run_info(time_of_execution, exitcode, taskid);
+    }
+}
+
+void run_tasks(s_task **tasks, uint64_t nb_tasks){
     for (int i = 0; i < nb_tasks; i++) {
+        fprintf(stdout, "%d\n", i);
+        // TODO ICI : je pense que tasks[i]->is_removed n'est pas initialise ?
         if(!(tasks[i]->is_removed)) {
             if (is_correct_timing(tasks[i]->t)) {
-                int status; // to store the return status of the child
-                int64_t time_of_execution = time(NULL);
                 int f = fork();
                 if (f == -1) {
                     perror("Fork error");
                     exit(EXIT_FAILURE);
-                } else if (f == 0) { // child that will execute the task
-                    move_stdout_stderr(i);
-                    char **tab = get_char_from_string(tasks[i]->command->argv, tasks[i]->command->argc);
-                    char *com = tab[0];
-                    execvp(com,tab);
-                } else {
-                    waitpid(f, &status, 0);
-                    if (WIFEXITED(status)) {
-                        // write exit code and time of run
-                     } else {
-                        perror("Le fils ne finit pas bien");
-                        exit(EXIT_FAILURE);
-                     }
+                } else if (f == 0) {
+                    // child that will launch the task so that saturnd doesn't block
+                    run_one_task(tasks[i], i);
+                    exit(0);
                 }
             }
         }
