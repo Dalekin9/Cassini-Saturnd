a68f8d36daa8060fc2f189150560c4a5272b4ec3
Laure Runser <laure-runser@tutanota.com>
2022-01-09 16:53:07 +0100

    Saturnd : - some refactorisation - the tasks are run from a child (which creates another child) so that saturnd doesn't hang - writes the exit code and the time of execution after the task is executed

--- src/saturnd.c
+++ src/saturnd.c
@@ -5,7 +5,6 @@ string **get_argv(uint32_t argc, int fd_a){
     int nb = 0;
     string **argv = malloc(nb * sizeof(string));
     for (int i = 0; i < argc;i++) {
-
         uint32_t t;
         int res = read(fd_a, &t, sizeof(uint32_t));
         if (res <= 0){
@@ -38,9 +37,9 @@ void find_if_removed(s_task* task, char* dir_path) {
 
     int ret = open(removed_path, O_RDONLY);
     if (ret == -1) {
-        task->is_removed = 0;
+        task->is_removed = false;
     } else {
-        task->is_removed = 1;
+        task->is_removed = true;
     }
     free(removed_path);
 }
@@ -66,49 +65,64 @@ void read_all_arguments(s_task *task, char* dir_path) {
     close(fd);
 }
 
-s_task **read_all_tasks() {
-    char *path = get_directory_tasks_path();
-    DIR *d = opendir(path);
-    struct dirent *entry;
-    s_task **all_tasks = malloc(1); // initial malloc otherwise realloc doesn't work
-    int nb_tasks = 0;
-    while ((entry = readdir(d))) {
-        if (strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0) {
-            // realloc the s_task** struct and malloc the new s_task*
-            all_tasks = realloc(all_tasks, (nb_tasks+1)*sizeof(s_task*));
-            all_tasks[nb_tasks] = malloc(sizeof(s_task));
-            is_malloc_error(all_tasks[nb_tasks]);
+// TODO ICI
+uint64_t read_max_id(char *dir_path) {
+    char *filename = "/last_taskid";
+    char *tmp = get_directory_path();
+    fprintf(stdout, "%s\n", tmp);
+    char *path = malloc(sizeof(filename) + sizeof(tmp) + 2);
+    strcpy(path, tmp); // les strcpy et strcat font de la merde
+    strcat(path, filename);
+    fprintf(stdout, "%s\n", path);
+    free(tmp);
 
-            // get the path of the directory (with the id of the task)
-            uint64_t id;
-            sscanf(entry->d_name, "%llu", &id); // get the id as a uint64
-            char* dir_path = get_directory_id_path(id);
-
-            // parse the infos
-            find_if_removed(all_tasks[nb_tasks], dir_path);
-            read_all_arguments(all_tasks[nb_tasks], dir_path);
-
-            free(dir_path);
-            nb_tasks += 1;
-        }
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        perror("Error opening the last_taskid file");
+        exit(EXIT_FAILURE);
     }
-    closedir(d);
+    uint64_t max_id;
+    read(fd, &max_id, sizeof(uint64_t));
+    close(fd);
     free(path);
-    return all_tasks;
+    return max_id;
+}
+
+uint64_t read_all_tasks(s_task** all_tasks) {
+    char *path = get_directory_tasks_path();
+    uint64_t max_id = read_max_id(path);
+
+    all_tasks = malloc((max_id + 1) * sizeof(s_task));
+    for (uint64_t i = 0; i < max_id + 1; i++) {
+        all_tasks[i] = malloc(sizeof(s_task));
+        is_malloc_error(all_tasks[i]);
+
+        // get the path of the directory (with the id of the task)
+        char* dir_path = get_directory_id_path(i);
+
+        // parse the infos
+        find_if_removed(all_tasks[i], dir_path);
+        read_all_arguments(all_tasks[i], dir_path);
+
+        free(dir_path);
+    }
+    free(path);
+    return max_id + 1;
 }
 
 int main(int argc, char * argv[]) {
      create_files(); // create the folders if needed
      create_pipes(); // create the pipes if needed
 
-     s_task** tasks = read_all_tasks(); // read all the tasks from disk
+     s_task** tasks;
+     uint64_t nb_tasks = read_all_tasks(tasks); // read all the tasks from disk
 
      char *pipes_directory = write_default_pipes_directory();
      char *request_pipe_name = get_pipe_name(pipes_directory, "saturnd-request-pipe");
 
      while (1) {
 
-          run_tasks();
+          run_tasks(tasks, nb_tasks);
           //sleep(5);
 
           int fd_req = open_pipe(request_pipe_name, O_RDONLY);
