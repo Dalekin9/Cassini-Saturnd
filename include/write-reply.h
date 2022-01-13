7f4ca1dfe0ec56beefa115b6b40f096c176aa87f
Laure Runser <laure-runser@tutanota.com>
2022-01-13 15:15:49 +0100

    Saturnd : fix function declaration in header for write_reply_std

--- include/write-reply.h
+++ include/write-reply.h
@@ -12,7 +12,7 @@
 #include "timing.h"
 
 void write_reply_c (struct timing *t, uint32_t length, string **s);
-void write_reply_std(uint64_t taskid, int is_stdout);
+void write_reply_std(uint64_t taskid, bool is_stdout);
 void write_reply_terminate();
 void write_reply_l(s_task **tasks, uint32_t nb_tasks);
 void write_reply_t_ec(uint64_t taskid);
